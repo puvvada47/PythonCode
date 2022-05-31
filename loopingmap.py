bf15d10bc70aa2e328a8cd51e4fdaadcf7db14f4
Viswanath <puvvada1210609147@gmail.com>
2022-05-31 19:57:07 +0530

    Scala code

---
+++ loopingmap.py
@@ -0,0 +1,5 @@
+dict = {"name" : "John", "age" : 36,"salary" : 50000, "desination":"softwareEngineer"}
+print("Dict value: ",dict)
+print("based on key:"+dict["name"])
+print("based on key:"+str(dict["age"]))
+print("based on key:"+str(dict["salary"]))
